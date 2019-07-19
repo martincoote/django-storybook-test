2f19a150bbe620320f290adcd25378c57e9d82a2
Martin Coote <martin@martincoote.com>
2019-07-19 22:54:38 +0100

    My furst regex

--- stories/index.js
+++ stories/index.js
@@ -6,10 +6,11 @@ import test from '../test.html'
 storiesOf('Test', module)
     .add('Test', () => {
         // Converts Django template tags to js template string
-        const dj_test = test.replace('{{', '${').replace('}}', '}');
+        const dj_test = test.replace(/{{/g, '${').replace(/}}/g, '}');
         //Defind page context objects as JSON.
         const page = {
-            title: 'hello jed'
+            title: 'hello jed',
+            description: 'A rain is a balloon\'s monkey. A dance sees a stepdaughter as a spacial dresser. A sycamore can hardly be considered a filthy walk without also being a mitten. Some posit the drafty beginner to be less than perished.'
         };
         // Return template tag as evaluated JS (template tags are replaced with context)
         return eval('`'+dj_test+'`');
