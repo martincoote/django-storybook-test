eef5d25fa1bd3aa2eb7db0d1b03dc623991cfdc5
Martin Coote <martin@martincoote.com>
2019-07-19 23:16:59 +0100

    Added Knobs (really story book were you developed by 5 year olds?

--- stories/index.js
+++ stories/index.js
@@ -1,17 +1,28 @@
 import { storiesOf } from '@storybook/html';
+import { withKnobs, text } from '@storybook/addon-knobs';
+
+const stories = storiesOf('Test stories', module);
+
+stories.addDecorator(withKnobs);
 
 //Imports .html file as a string.
 import test from '../test.html'
 
-storiesOf('Test', module)
-    .add('Test', () => {
+stories.add('Test', () => {
         // Converts Django template tags to js template string
         const dj_test = test.replace(/{{/g, '${').replace(/}}/g, '}');
         //Defind page context objects as JSON.
         const page = {
-            title: 'hello jed',
-            description: 'A rain is a balloon\'s monkey. A dance sees a stepdaughter as a spacial dresser. A sycamore can hardly be considered a filthy walk without also being a mitten. Some posit the drafty beginner to be less than perished.'
+            title: text('Title', 'hello jed'),
+            description: text(
+                'Description',
+                'A rain is a balloon\'s monkey. A dance sees a stepdaughter as a spacial dresser. ' +
+                'A sycamore can hardly be considered a filthy walk without also being a mitten. ' +
+                'Some posit the drafty beginner to be less than perished.'
+            )
         };
         // Return template tag as evaluated JS (template tags are replaced with context)
         return eval('`'+dj_test+'`');
     });
+
+stories.add('Knob', () => text('test Text', 'edit me'))
